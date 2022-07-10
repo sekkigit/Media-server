7cf16fa602010d707bacbbd112fece54be848e22
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-07-10 17:19:41 +0200

    1

--- init.sh
+++ init.sh
@@ -209,7 +209,7 @@ cat <<EOF >> /home/"$USER"/backup-task/backup-daily.sh
 #!/bin/bash
 
 docker-compose -f /home/"$USER"/docker/docker-compose.yml pause
-tar --exclude=/home/"$USER"/docker/qbit/config/qBittorrent/ipc-socket -zcf /home/"$USER"/backup/daily/backup-$(date +%Y%m%d).tar.gz -C /home/"$USER"/docker/*
+tar --exclude=/home/"$USER"/docker/qbit/config/qBittorrent/ipc-socket -zcf /home/"$USER"/backup/daily/backup-\$(date +%Y%m%d).tar.gz -C /home/"$USER"/docker/*
 find /home/"$USER"/backup/daily/* -mtime +7 -delete
 docker-compose -f /home/"$USER"/docker/docker-compose.yml unpause
 EOF
@@ -219,7 +219,7 @@ cat <<EOF >> /home/"$USER"/backup-task/backup-weekly.sh
 #!/bin/bash
 
 docker-compose -f /home/"$USER"/docker/docker-compose.yml pause
-tar --exclude=/home/"$USER"/docker/qbit/config/qBittorrent/ipc-socket -zcf /home/"$USER"/backup/weekly/backup-$(date +%Y%m%d).tar.gz -C /home/"$USER"/docker/*
+tar --exclude=/home/"$USER"/docker/qbit/config/qBittorrent/ipc-socket -zcf /home/"$USER"/backup/weekly/backup-\$(date +%Y%m%d).tar.gz -C /home/"$USER"/docker/*
 find /home/"$USER"/backup/weekly/* -mtime +31 -delete
 docker-compose -f /home/"$USER"/docker/docker-compose.yml unpause
 EOF
@@ -229,7 +229,7 @@ cat <<EOF >> /home/"$USER"/backup-task/backup-monthly.sh
 #!/bin/bash
 
 docker-compose -f /home/"$USER"/docker/docker-compose.yml pause
-tar --exclude=/home/"$USER"/docker/qbit/config/qBittorrent/ipc-socket -zcf /home/"$USER"/backup/monthly/backup-$(date +%Y%m%d).tar.gz -C /home/"$USER"/docker/*
+tar --exclude=/home/"$USER"/docker/qbit/config/qBittorrent/ipc-socket -zcf /home/"$USER"/backup/monthly/backup-\$(date +%Y%m%d).tar.gz -C /home/"$USER"/docker/*
 find /home/"$USER"/backup/monthly/* -mtime +365 -delete
 docker-compose -f /home/"$USER"/docker/docker-compose.yml unpause
 EOF
